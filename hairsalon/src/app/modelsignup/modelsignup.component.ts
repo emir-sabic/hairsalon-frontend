12a98d932c88baa519171a64b6182f28afad5ce1
Emir <emir.sabic@stu.ssst.edu.ba>
2023-04-01 18:33:37 +0200

    Bookappointment and modelsignup pages

---
+++ hairsalon/src/app/modelsignup/modelsignup.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-modelsignup',
+  templateUrl: './modelsignup.component.html',
+  styleUrls: ['./modelsignup.component.css']
+})
+export class ModelsignupComponent {
+
+}
