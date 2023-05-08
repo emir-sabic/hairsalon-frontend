b4f391913b253a0ded3f12a9bd5637cf0a5936bf
Emir <emir.sabic@stu.ssst.edu.ba>
2023-05-09 00:56:42 +0200

    changes on routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-import { BookapointmentComponent } from './bookapointment/bookapointment.component'
-import { HomepageComponent } from './homepage/homepage.component';
-import { LoginsingupComponent } from './loginsingup/loginsingup.component';
-import { ModelsignupComponent } from './modelsignup/modelsignup.component';
-import { ServicesComponent } from './services/services.component';
-import { SignupComponent } from './signup/signup.component';
+import { BookapointmentComponent } from './featuers/bookapointment/bookapointment.component'
+import { HomepageComponent } from './featuers/homepage/homepage.component';
+import { LoginsingupComponent } from './featuers/loginsingup/loginsingup.component';
+import { ModelsignupComponent } from './featuers/modelsignup/modelsignup.component';
+import { ServicesComponent } from './featuers/services/services.component';
+import { SignupComponent } from './featuers/signup/signup.component';
 
 const routes: Routes = [
 {path:'', component:HomepageComponent},
@@ -14,7 +14,7 @@ const routes: Routes = [
 {path:'login', component:LoginsingupComponent},
 {path:'modelsignup', component:ModelsignupComponent},
 {path:'signup', component:SignupComponent},
-{path:'book', component:BookapointmentComponent}
+{path:'appointment', component:BookapointmentComponent}
 ];
 
 @NgModule({
